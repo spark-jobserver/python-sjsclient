da2acff36ee000142a919122bd7731c8a48f09bd
Noorul Islam K M <noorul@noorul.com>
2015-11-25 09:13:29 +0530

    Issue #3: Update documentation config

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,16 +1,4 @@
 # -*- coding: utf-8 -*-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import os
 import sys
@@ -23,7 +11,6 @@ sys.path.insert(0, os.path.abspath('../..'))
 extensions = [
     'sphinx.ext.autodoc',
     #'sphinx.ext.intersphinx',
-    'oslosphinx'
 ]
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
@@ -38,7 +25,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'python-sjsclient'
-copyright = u'2013, OpenStack Foundation'
+copyright = u'2015, Hewlett Packard Enterprise Development LP'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
@@ -67,8 +54,7 @@ htmlhelp_basename = '%sdoc' % project
 latex_documents = [
     ('index',
      '%s.tex' % project,
-     u'%s Documentation' % project,
-     u'OpenStack Foundation', 'manual'),
+     u'%s Documentation' % project),
 ]
 
 # Example configuration for intersphinx: refer to the Python standard library.
