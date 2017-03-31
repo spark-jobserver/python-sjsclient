8b9d6a62715657a96294da18c5dfac71c2308f8b
Noorul Islam K M <noorul@noorul.com>
2017-03-31 08:17:30 +0530

    Style fixes

--- sjsclient/tests/unit/test_context.py
+++ sjsclient/tests/unit/test_context.py
@@ -9,89 +9,98 @@ from sjsclient import context
 from sjsclient import exceptions
 from sjsclient import utils
 
-context_name = "test-context"
-context_create_response = "OK"
-context_repr = "<Context: %s>" % context_name
-context_get_response = """
+CONTEXT_NAME = "test-context"
+CONTEXT_CREATE_RESPONSE = "OK"
+CONTEXT_REPR = "<Context: %s>" % CONTEXT_NAME
+CONTEXT_GET_RESPONSE = """
 [
 "%s"
 ]
-""" % context_name
+""" % CONTEXT_NAME
 
 
 class TestContext(base.BaseTestCase):
+    """Unit tests for context resource"""
 
     TEST_ENDPOINT = 'http://example.com:8090'
 
     def setUp(self):
+        """Test suite setup"""
         super(TestContext, self).setUp()
         self.client = client.Client(self.TEST_ENDPOINT)
 
-    def assertContextFields(self, test_ctx):
-        self.assertEqual(context_name, test_ctx.name)
-        self.assertEqual(context_repr, repr(test_ctx))
+    def assert_ctx_fields(self, test_ctx):
+        """Helper method for asserts"""
+        self.assertEqual(CONTEXT_NAME, test_ctx.name)
+        self.assertEqual(CONTEXT_REPR, repr(test_ctx))
         self.assertIsInstance(test_ctx, context.Context)
 
     @requests_mock.Mocker()
     def test_create(self, mock_req):
+        """Test create context"""
         post_url = utils.urljoin(self.TEST_ENDPOINT,
                                  self.client.contexts.base_path,
-                                 context_name)
+                                 CONTEXT_NAME)
 
-        mock_req.post(post_url, text=context_create_response)
+        mock_req.post(post_url, text=CONTEXT_CREATE_RESPONSE)
 
         params = {'num-cpu-cores': '4',
                   'memory-per-node': '512m'}
 
-        test_context = self.client.contexts.create(context_name, params)
-        self.assertContextFields(test_context)
+        test_context = self.client.contexts.create(CONTEXT_NAME, params)
+        self.assert_ctx_fields(test_context)
 
     @requests_mock.Mocker()
     def test_list(self, mock_req):
+        """Test list contexts"""
         get_url = utils.urljoin(self.TEST_ENDPOINT,
                                 self.client.contexts.base_path)
 
-        mock_req.get(get_url, text=context_get_response)
+        mock_req.get(get_url, text=CONTEXT_GET_RESPONSE)
 
         ctx_list = self.client.contexts.list()
-        self.assertContextFields(next(ctx_list))
+        self.assert_ctx_fields(next(ctx_list))
 
     @requests_mock.Mocker()
     def test_get(self, mock_req):
+        """Test get context"""
         get_url = utils.urljoin(self.TEST_ENDPOINT,
                                 self.client.contexts.base_path)
 
-        mock_req.get(get_url, text=context_get_response)
+        mock_req.get(get_url, text=CONTEXT_GET_RESPONSE)
 
-        test_ctx = self.client.contexts.get(context_name)
-        self.assertContextFields(test_ctx)
+        test_ctx = self.client.contexts.get(CONTEXT_NAME)
+        self.assert_ctx_fields(test_ctx)
 
     @requests_mock.Mocker()
     def test_get_non_existing(self, mock_req):
+        """Test get non existing context"""
         get_url = utils.urljoin(self.TEST_ENDPOINT,
                                 self.client.contexts.base_path)
 
-        mock_req.get(get_url, text=context_get_response)
+        mock_req.get(get_url, text=CONTEXT_GET_RESPONSE)
         self.assertRaises(exceptions.NotFoundException,
                           self.client.contexts.get, 'does-not-exist')
 
     @requests_mock.Mocker()
     def test_delete(self, mock_req):
+        """Test context delete"""
         get_url = utils.urljoin(self.TEST_ENDPOINT,
                                 self.client.contexts.base_path,
-                                context_name)
+                                CONTEXT_NAME)
 
-        mock_req.delete(get_url, text=context_create_response)
-        self.client.contexts.delete(context_name)
+        mock_req.delete(get_url, text=CONTEXT_CREATE_RESPONSE)
+        self.client.contexts.delete(CONTEXT_NAME)
 
     @requests_mock.Mocker()
     def test_context_instance_delete(self, mock_req):
+        """Test delete method of context instace"""
         get_url = utils.urljoin(self.TEST_ENDPOINT,
                                 self.client.contexts.base_path)
         del_url = utils.urljoin(self.TEST_ENDPOINT,
                                 self.client.contexts.base_path,
-                                context_name)
-        mock_req.get(get_url, text=context_get_response)
-        test_ctx = self.client.contexts.get(context_name)
-        mock_req.delete(del_url, text=context_create_response)
+                                CONTEXT_NAME)
+        mock_req.get(get_url, text=CONTEXT_GET_RESPONSE)
+        test_ctx = self.client.contexts.get(CONTEXT_NAME)
+        mock_req.delete(del_url, text=CONTEXT_CREATE_RESPONSE)
         test_ctx.delete()
